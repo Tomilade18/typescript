6e8b3c547ac900cb16ce7623cd0179a1b7909bda
Tomilade18 <jboyyumzey>
2023-06-02 12:01:12 +0100

    learning arrays

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-let sales = 123_456_789;
-let course = 'Typescript';
-let is_published = true;
+//arrays
+//let numbers = [1, 2, 3];
+// the any array
+// let numbers = [];
+// numbers[0] = 1;
+// numbers[1] = "1";
+// if you want to use an empty array you ahve to specify
+//the array type 
 
-  // type any, as of best pratice avoid using the any type as much as possible
-  let level;
+
+
+// benefit of using typescript
+// intellesense or code completion
+// bbecause our editor know the type of end it offer code completion
+let numbers: number[] = [];
+numbers.forEach(n => n.)
