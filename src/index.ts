d57341f88361c0ee0a08ed2b7f4a72130b4b2404
Tomilade18 <jboyyumzey@gmail.com>
2023-06-05 12:17:14 +0100

    learning type alias

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 // objects
 //how to define a methiod in this object
 //every object should have a return method
-let employee: {
+type Employee = {
   readonly id: number,
     name: string,
     retire: (date: Date) => void
-    
-} = {id: 1,
+} 
+
+let employee: Employee = {
+    id: 1,
      name: 'tomi',
     retire: (date: Date) => {
 console.log(date);
     }
-   };
+    };
 
