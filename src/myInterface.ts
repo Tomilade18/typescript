6b5d47cce544af0120a051d48d62b6d17d2151fa
Tomilade18 <jboyyumzey@gmail.com>
2023-06-06 18:59:34 +0100

    learning interface extension and inheritance

--- src/myInterface.ts
+++ src/myInterface.ts
@@ -8,8 +8,18 @@ interface User {
    getCoupon(couponname: string, value: number): number
 }
 
-const hitesh: User = {dbId: 22, email: "h@h.com",
- userId: 2211,
+interface User {
+    githubToken: string
+}
+
+interface Admin extends User {
+    role: "admin" | "ta" | "learner"
+}
+
+const hitesh: Admin = {dbId: 22, email: "h@h.com",
+role: "admin",
+userId: 2211,
+ githubToken: "github",
 startTrail: () => {
     return "trail started"
 },
