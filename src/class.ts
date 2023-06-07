80f117c9f5a88c0b819686b613265f854da6f191
Tomilade18 <jboyyumzey@gmail.com>
2023-06-07 17:14:24 +0100

    learning private public

--- src/class.ts
+++ src/class.ts
@@ -1,12 +1,21 @@
+// class User {
+//    public email: string
+//    private name: string
+//    readonly city: string = "Abuja"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+
 class User {
-    email: string
-    name: string
-    city: string = ""
-    constructor(email: string, name: string){
-        this.email = email;
-        this.name = name
-    }
-}
+readonly city: string = "lagos"
+     constructor(
+       public email: string,
+       public name: string,
+      // private userId: string
+       ){}
+ }
 
 const tomilade = new User("h@h.com", "hitesh")
-hitesh.city = "Lagos"
+hitesh.name
